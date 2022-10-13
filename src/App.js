aeaf39dfd01763a12456233561981ffa4f8ab688
AdiSemper <semperag@dukes.jmu.edu>
2022-10-13 01:41:05 -0400

    adding api data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import styles from './App.css';
 
 function App() {
+  /*
+  const [data, setData] = useState({})
+  
+  
+  const url = "https://restcountries.com/v3.1/all";
+
+  axios.get(url).then((response) => {
+    setData(response.data)
+  })
+*/
+const url = 'https://restcountries.com/v3.1/name/japan';
+const [countries, setCountries] = useState([])
+
+  const fetchCountryData = async() => {
+    const response = await fetch(url)
+    const countries = await response.json()
+    setCountries(countries)
+    console.log(countries);
+  }
+
+  useEffect(() => {
+    fetchCountryData()
+}, [])
+
+const Countries = () => {
+  const [countries, setCountries] = useState([])
+
+  const fetchCountryData = async() => {
+    const response = await fetch(url)
+    const countries = await response.json()
+    setCountries(countries)
+    console.log(countries);
+  }
+
+  useEffect(() => {
+    fetchCountryData()
+}, [])
+}
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
+        <div id="navbar">
+          <span>Where in the World?</span>
+          <span>Light Mode</span>
+        </div>
+
+        <div id="filter">
+          <div id="search">search</div>
+          <div id="region-filter">filter by region</div>
+        </div>
       </header>
+
+      <main id="all-countries">
+        <div id="pharse">In the world we have 250 countries</div>
+        <div className="country">
+          <img src="https://flagcdn.com/w320/us.png"></img>
+          <div className="country-name">United States of America</div>
+          <div className="quick-facts">
+            <div className="population">
+              <span className="title">Population: </span>
+              <span>329484123</span>
+            </div>
+            <div className="region">
+              <span className="title">Region: </span>
+              <span>Americas</span>
+            </div>
+            <div className="capital">
+              <span className="title">Capital: </span>
+              <span>Washington, D.C.</span>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <div>{/*scountries.map(country) => {
+          const { id } = country
+
+          return (
+            <article 
+          )
+        })*/}
+        </div>
+      </footer>
     </div>
   );
 }
