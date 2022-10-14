53b56f7cbe33c87f6a28e228d9e1f844781ac72e
AdiSemper <semperag@dukes.jmu.edu>
2022-10-13 23:58:53 -0400

    inserting variables

--- src/Country.js
+++ src/Country.js
@@ -1,12 +1,15 @@
 const Country = () => {
+    const name = "United States of America";
+    const population = 329484123;
+
     return (
         <div className="country">
           <img src="https://flagcdn.com/w320/us.png"></img>
-          <div className="country-name">United States of America</div>
+          <div className="country-name">{name}</div>
           <div className="quick-facts">
             <div className="population">
               <span className="title">Population: </span>
-              <span>329484123</span>
+              <span>(population)</span>
             </div>
             <div className="region">
               <span className="title">Region: </span>
