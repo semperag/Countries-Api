91dd2babe83cf3453bac179cf5e22cb1bd66a796
AdiSemper <semperag@dukes.jmu.edu>
2022-10-14 00:19:11 -0400

    making countries dynamic

--- src/Country.js
+++ src/Country.js
@@ -1,4 +1,5 @@
 const Country = () => {
+    let image = "https://flagcdn.com/w320/us.png"
     let name = "United States of America";
     let population = 329484123;
     let region = "Americas";
@@ -6,20 +7,22 @@ const Country = () => {
 
     return (
         <div className="country">
-          <img src="https://flagcdn.com/w320/us.png"></img>
-          <div className="country-name">{name}</div>
-          <div className="quick-facts">
-            <div className="population">
-              <span className="title">Population: </span>
-              <span>{population}</span>
-            </div>
-            <div className="region">
-              <span className="title">Region: </span>
-              <span>{region}</span>
-            </div>
-            <div className="capital">
-              <span className="title">Capital: </span>
-              <span>{capital}</span>
+          <img src={image}></img>
+          <div className="country-container">
+            <div className="country-name">{name}</div>
+            <div className="quick-facts">
+                <div className="population">
+                <span className="title">Population: </span>
+                <span>{population}</span>
+                </div>
+                <div className="region">
+                <span className="title">Region: </span>
+                <span>{region}</span>
+                </div>
+                <div className="capital">
+                <span className="title">Capital: </span>
+                <span>{capital}</span>
+                </div>
             </div>
           </div>
         </div>
