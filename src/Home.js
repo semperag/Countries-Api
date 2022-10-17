3f6354938e343273bdd9591037cd8f7dfc2b74d8
AdiSemper <semperag@dukes.jmu.edu>
2022-10-16 23:42:50 -0400

    making code reusable

--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,19 @@
 import { getByPlaceholderText } from '@testing-library/react';
 import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
     //let name = 'mario';
 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPendings] = useState(true);
-    const [error, setError] = useState(null);
+    const {data: blogs, isPending, error} =  useFetch('http://localhost:8000/blogs');
 
     const [name, setName] = useState('mario');
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs)
-    }
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-            
-            .then(res => {
-                if (!res.ok) {
-                    throw Error('could not fetch the data for that resource')
-                }
-                return res.json()
-            })
-            .then(data => {
-                console.log(data);
-                setBlogs(data);
-                setIsPendings(false)
-            })
-            .catch(err => {
-                setIsPendings(false)
-                setError(err.message);
-            })
-        }, 1000)
-    }, []);
+    }    
 
     const [age, setAge] = useState(25);
 
