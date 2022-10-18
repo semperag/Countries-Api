ebd1beec5240971eca4b7e7974a5e86f13380556
AdiSemper <semperag@dukes.jmu.edu>
2022-10-17 23:22:28 -0400

    more practice - switch and router

--- src/App.js
+++ src/App.js
@@ -4,33 +4,51 @@ import styles from './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 import Country from './Country';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Create from './Create';
 
 function App() {
 
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
 
-      <Navbar />
-      <header>
+        <Navbar />
+          <div className='content'>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/create">
+                <Create />
+              </Route>
+            </Switch>
+          </div>
+        <header>
 
-        <div id="navbar">
-          <span>Where in the World?</span>
-          <span>Light Mode</span>
-        </div>
-        <Home />
-        <div id="filter">
-          <div id="search">search</div>
-          <div id="region-filter">filter by region</div>
-        </div>
-      </header>
+{/*
+          <div id="navbar">
+            <span>Where in the World?</span>
+            <span>Light Mode</span>
+          </div>
+          <div id="filter">
+            <div id="search">search</div>
+            <div id="region-filter">filter by region</div>
+          </div>
+        </header>
 
-      <main id="all-countries">
-        <div id="pharse">In the world we have 250 countries</div>
-        <Country />
-      </main>
-      <footer>
-      </footer>
+        <main id="all-countries">
+          <div id="pharse">In the world we have 250 countries</div>
+          <Country />
+        </main>
+        <footer>
+        </footer>
+      </div>
+
+  */}
+    </header>
     </div>
+    </Router>
   );
 }
 
