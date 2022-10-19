daa0a9a323087b115c406b1cdc0f66488edfd843
AdiSemper <semperag@dukes.jmu.edu>
2022-10-19 00:07:51 -0400

    adding route for invalid urls

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Country from './Country';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
 
@@ -25,6 +26,9 @@ function App() {
               </Route>
               <Route path="/blogs/:id">
                 <BlogDetails />
+              </Route>
+              <Route path="*">
+                <NotFound />
               </Route>
             </Switch>
           </div>
