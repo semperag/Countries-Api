e3d652dc4dfd005d49d1e698d6bdbf69436845e2
AdiSemper <semperag@dukes.jmu.edu>
2022-10-18 22:45:41 -0400

    Linking to different blogs

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({blogs, title, handleDelete}) => {
     //const blogs = props.blogs;
     //const title = props.title;
@@ -10,9 +12,11 @@ const BlogList = ({blogs, title, handleDelete}) => {
             <h2> {title} </h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                    <button onClick={() => handleDelete(blog.id)}>delete blog</button>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{ blog.title }</h2>
+                        <p>Written by { blog.author }</p>
+                        <button onClick={() => handleDelete(blog.id)}>delete blog</button>
+                    </Link>
                 </div>
             ))}
         </div>
