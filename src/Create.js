81358ac0f0f01155222c1b14d7275a6a9076ead3
AdiSemper <semperag@dukes.jmu.edu>
2022-10-18 23:49:03 -0400

    adding new blog dynamically

--- src/Create.js
+++ src/Create.js
@@ -4,12 +4,23 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = {title, body, author};
 
+        setIsPending(true);
+
         console.log(blog);
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added');
+            setIsPending(false);
+        })
     }
 
     return (
@@ -37,7 +48,8 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding blog...</button>}
                 <p>{title}</p>
                 <p>{body}</p>
                 <p>{author}</p>
