6f099da0871869852eabbf7744636a156109cd9c
AdiSemper <semperag@dukes.jmu.edu>
2022-10-20 22:22:35 -0400

    adding link to paths

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,10 +3,8 @@ import {Link} from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>The Dojo Blog</h1>
             <div className="links">
-                <Link to="/">Home</Link>
-                <Link to="/create">New Blog</Link>
+                <Link to="/">Where in the world?</Link>
             </div>
         </nav>
     );
