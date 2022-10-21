23bfb2f348d22613652813c41571bd40683ae7c5
AdiSemper <semperag@dukes.jmu.edu>
2022-10-20 22:21:23 -0400

    adding select and options

---
+++ src/SearchBar.js
@@ -0,0 +1,22 @@
+
+
+const SearchBar = () => {
+    return(
+        <div className="searchbar">
+            <input placeholder="search for a country"></input>
+            <div className="filterbar">
+                <div>Filter by Region</div>
+                <select defaultValue={"All counties"}>
+                    <option value="All countries">All countries</option>
+                    <option value="Africa">Africa</option>
+                    <option value="America">America</option>
+                    <option value="Asia">Asia</option>
+                    <option value="Europe">Europe</option>
+                    <option value="Oceania">Oceania</option>
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export default SearchBar;
