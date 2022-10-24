20d409a986177ee16b58ca5dea880759229851f8
AdiSemper <semperag@dukes.jmu.edu>
2022-10-24 14:56:21 -0400

    adding in more api data

--- src/Country.js
+++ src/Country.js
@@ -3,27 +3,59 @@ import useFetch from "./useFetch";
 
 const Country = () => {
   const {id} = useParams();
+  console.log(id);
   const {data: country, error, isPending} = useFetch('https://restcountries.com/v3.1/name/' + id);
-
+  country && console.log("name = " + JSON.stringify(country[0].currencies));
+    if (country && country[0]) {
+     for (let curr of country[0].currencies) {
+      console.log("name = " + curr.name);
+     } 
+    }
     return (
-        <div className="country">
-          {country && <img src={country.image}></img> }
-          <div className="country-container">
-              {country && <div className="country-name title">{country.name}</div>}
-              <div className="quick-facts">
+        <div className="country-details">
+          {country && <img src={country[0].flags.png}></img> }
+          <div className="country-info">
+            {country && <div className="country-name title">{country[0].name.common}</div>}
+            <div className="country-facts">
+              <div className="facts-left">
+                <div className="native-name">
+                  <span className="title">Native Name: </span>
+                  {country && <span>{country[0].population}</span>}
+                </div>
                 <div className="population">
-                {country && <span className="title">Population: {country.population}</span>}
-                <span></span>
+                  <span className="title">Population: </span>
+                  {country && <span>{country[0].population}</span>}
                 </div>
                 <div className="region">
-                {country && <span className="title">Region: {country.region}</span>}
-                <span></span>
+                  <span className="title">Region: </span>
+                  {country && <span>{country[0].region}</span>}
+                </div>
+                <div className="sub-region">
+                  <span className="title">Sub Region: </span>
+                  {country && <span>{country[0].subregion}</span>}
                 </div>
                 <div className="capital">
-                {country && <span className="title">Capital: {country.captial}</span>}
-                <span></span>
+                  <span className="title">Capital: </span>
+                  {country && <span>{country[0].capital}</span>}
                 </div>
+              </div>
+              <div className="facts-right">
+                <div className="domain">
+                  <span className="title">Top Level Domain: </span>
+                  {country && <span>{country[0].tld[0]}</span>}
+                </div>
+                <div className="currency">
+                  <span className="title">Currency: </span>
+                  {country && <span>{country[0].population}</span>}
+                </div>
+                <div className="Languages">
+                  <span className="title">Languages: </span>
+                  {country && <span>{country[0].capital}</span>}
+                </div>
+              </div>
             </div>
+            <div>Border Countries:</div>
+            <div>these are countries</div>
           </div>
         </div>
     );
