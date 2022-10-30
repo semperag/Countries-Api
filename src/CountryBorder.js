358729a8e1f860738ca24276d5f0b9fa6621cd38
AdiSemper <semperag@dukes.jmu.edu>
2022-10-29 23:51:52 -0400

    making file js

---
+++ src/CountryBorder.js
@@ -0,0 +1,10 @@
+function CountryBorder({countries}) {
+    return (
+        <span>border</span>
+        {countries.map((country) => (
+            <div></div>
+        ))}
+    );
+}
+
+export default CountryBorder;
