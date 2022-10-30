72004a71c26e4aa426751cb670be0202e8ff7f36
AdiSemper <semperag@dukes.jmu.edu>
2022-10-30 03:35:53 -0400

    adding in new api

--- src/Country.js
+++ src/Country.js
@@ -1,28 +1,31 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 import CountryBorder from './CountryBorder';
+import CountryCurrency from "./CountryCurrency";
 
 const Country = () => {
   const {id} = useParams();
   console.log(id);
-  const {data: country, error, isPending} = useFetch('https://restcountries.com/v3.1/name/' + id);
-  country && console.log("currency = " + JSON.stringify(country[0].currencies));
-  country && console.log("currency = " + JSON.stringify(country[0].borders));
+  const {data: country, error, isPending} = useFetch('https://restcountries.com/v2/alpha?codes=' + id);
+  country && console.log("currency = " + JSON.stringify(country[0].currencies[0].name));
+  country && console.log("borders = " + JSON.stringify(country[0].borders));
+  country && console.log(country);
     if (country && country[0]) {
-      for (let i = 0; i < country[0].borders.length; i++) {
-        console.log(country[0].borders[i]);
-      }
+      //for (let i = 0; i < country[0].borders.length; i++) {
+      //  console.log(country[0].borders[i]);
+      //}
+      console.log(country);
     }
     return (
         <div className="country-details">
           {country && <img src={country[0].flags.png}></img> }
           <div className="country-info">
-            {country && <div className="country-name title">{country[0].name.common}</div>}
+            {country && <div className="country-name title">{country[0].name}</div>}
             <div className="country-facts">
               <div className="facts-left">
                 <div className="native-name">
                   <span className="title">Native Name: </span>
-                  {country && <span>{country[0].population}</span>}
+                  {country && <span>{country[0].nativeName}</span>}
                 </div>
                 <div className="population">
                   <span className="title">Population: </span>
@@ -44,11 +47,11 @@ const Country = () => {
               <div className="facts-right">
                 <div className="domain">
                   <span className="title">Top Level Domain: </span>
-                  {country && <span>{country[0].tld[0]}</span>}
+                  {country && <span>{country[0].topLevelDomain[0]}</span>}
                 </div>
                 <div className="currency">
                   <span className="title">Currency: </span>
-                  {country && <span>{country[0].population}</span>}
+                  {country && country[0].currencies[0].name}
                 </div>
                 <div className="Languages">
                   <span className="title">Languages: </span>
@@ -58,7 +61,6 @@ const Country = () => {
             </div>
             <div>Border Countries:</div>
             {country && <CountryBorder countries={country[0].borders}/>}
-            <div>these are countries</div>
           </div>
         </div>
     );
