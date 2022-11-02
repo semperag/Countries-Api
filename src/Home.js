8f3b5795647088313808dc519251009a7b5406b6
AdiSemper <semperag@dukes.jmu.edu>
2022-11-02 18:07:36 -0400

    renaming api

--- src/Home.js
+++ src/Home.js
@@ -5,14 +5,40 @@ import CountryList from './CountryList';
 import useFetch from './useFetch';
 
 const Home = () => {
-    const {data: countries, isPending, error} =  useFetch('https://restcountries.com/v3.1/all');
+    const [region, setRegion] = useState('all');
+
+    const {data: countries, isPending, error} =  useFetch('https://restcountries.com/v3.1/' + region);
+
+    const selectRegion = (e) => {
+        if (e.target.value === "all") {
+            setRegion('all');
+        }
+        else {
+            setRegion('region/' + e.target.value);
+        }
+    }
 
     return (
+        <><div className="searchbar">
+            <input placeholder="search for a country"></input>
+            <div className="filterbar">
+                <div>Filter by Region</div>
+                <select defaultValue={"all"} onChange={selectRegion}>
+                    <option value="all">All countries</option>
+                    <option value="Africa">Africa</option>
+                    <option value="America">America</option>
+                    <option value="Asia">Asia</option>
+                    <option value="Europe">Europe</option>
+                    <option value="Oceania">Oceania</option>
+                </select>
+            </div>
+        </div>
+        
         <div className="home">
 
-            {countries && <CountryList countries={countries}/>}
-            {console.log('after')}
-        </div>
+                {countries && <CountryList countries={countries}/>}
+                {console.log('after')}
+            </div></>
     );
 }
 
