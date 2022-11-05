d724cf2774effeabc9b955f4bf9a752414cae869
AdiSemper <semperag@dukes.jmu.edu>
2022-11-04 23:53:15 -0400

    unused file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Create from './Create';
-import BlogDetails from './BlogDetails';
 import NotFound from './NotFound';
 import SearchBar from './SearchBar';
 import Country from './Country';
