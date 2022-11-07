67c90ed4779db5a194f028605ed201abbe461bfd
AdiSemper <semperag@dukes.jmu.edu>
2022-11-06 23:37:23 -0500

    adding file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import styles from './App.css';
+import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
