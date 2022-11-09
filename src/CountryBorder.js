e885c5554d82b5cbb17d01aa83d5db54a93920bb
AdiSemper <semperag@dukes.jmu.edu>
2022-11-08 23:23:18 -0500

    right format

--- src/CountryBorder.js
+++ src/CountryBorder.js
@@ -5,9 +5,9 @@ function CountryBorder({countries, name}) {
     return (
         <div className="border-list">
             {countries && countries.map((country) => (
-                    <Link to={`/details/${country}`} key={country}>
-                        {country}
-                    </Link>
+                <Link to={`/details/${country}`} key={country}>
+                    {country}
+                </Link>
             ))}
         </div>
     );
