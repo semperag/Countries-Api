a4e71030bd8cea28de1271e3edf7362f9ac297bc
AdiSemper <semperag@dukes.jmu.edu>
2022-11-10 00:04:56 -0500

    back function works

--- src/Back.js
+++ src/Back.js
@@ -1,9 +1,15 @@
 import './Back.css'
+import { useHistory } from "react-router-dom";
 
 const Back = () => {
+    const history = useHistory();
+
+    const goBack = (e) => {
+        history.go(-1);
+    }
     return (
         <div className="back">
-            <button>Back</button>
+            <button onClick={goBack}>Back</button>
         </div>
     )
 }
