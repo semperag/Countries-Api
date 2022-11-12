b16e9e2d5854ad0481ca587b5fe92b6842168f11
AdiSemper <semperag@dukes.jmu.edu>
2022-11-11 21:14:51 -0500

    working on dark mode

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,23 @@
 import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
-const Navbar = () => {
+const Navbar = ({theme, setTheme}) => {
+       const toggleTheme = () => {
+        setTheme({ ...theme, isLightTheme: !theme.isLightTheme });
+       };
+    
     return (
-        <nav className="navbar">
-            <div className="links">
-                <Link to="/">Where in the world?</Link>
-            </div>
-        </nav>
+        <Navbar value={{
+            ...theme,
+            toggleTheme
+           }}>
+            <nav className={`navbar ${theme}`}>
+                <div className="links">
+                    <Link to="/">Where in the world?</Link>
+                    <button onClick={toggleTheme}>light mode</button>
+                </div>
+            </nav>
+        </Navbar>
     );
 }
 
