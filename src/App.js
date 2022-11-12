d9cec23505c1d7b0132160dcaff95599614d71bc
AdiSemper <semperag@dukes.jmu.edu>
2022-11-11 21:14:56 -0500

    working on dark mode

--- src/App.js
+++ src/App.js
@@ -10,29 +10,45 @@ import NotFound from './NotFound';
 import Country from './Country';
 
 function App() {
+  const [theme, setTheme] = useState(
+    localStorage.getItem('theme') || 'light'
+  );
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+    document.body.className = theme;
+  }, [theme]);
 
   return (
-    <Router>
-      <Navbar />
-      <div className="App">
-          <div className='content'>
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/details/:id">
-                <div className='single-country'>
-                  <Back />
-                  <Country />
-                </div>
-              </Route>
-              <Route path="*">
-                <NotFound />
-              </Route>
-            </Switch>
-          </div>
+    <div className="Body">
+      <Router>
+        <Navbar theme={theme} setTheme={setTheme}/>
+        <div className="App">
+            <div className='content'>
+              <Switch>
+                <Route exact path="/">
+                  <Home/>
+                </Route>
+                <Route path="/details/:id">
+                  <div className='single-country'>
+                    <Back />
+                    <Country />
+                  </div>
+                </Route>
+                <Route path="*">
+                  <NotFound />
+                </Route>
+              </Switch>
+            </div>
+      </div>
+      </Router>
     </div>
-    </Router>
   );
 }
 
