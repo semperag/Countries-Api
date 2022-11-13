cf0e477bf544c0a0cfb802fd16d455598cc9b456
AdiSemper <semperag@dukes.jmu.edu>
2022-11-13 00:46:05 -0500

    dark mode

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,27 @@
 import {Link} from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom'
+import fontawesome from '@fortawesome/fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon } from '@fortawesome/free-regular-svg-icons'
+import 'font-awesome/css/font-awesome.min.css';
 
-const Navbar = () => {
-       
+import './Navbar.css';
+
+const Navbar = ({theme, setTheme}) => {
+    const toggleTheme = () => {
+        if (theme === 'light') {
+          setTheme('dark');
+        } else {
+          setTheme('light');
+        }
+      };
     
     return (
-            <nav className={`navbar`}>
+            <nav className={`navbar ${theme}`}>
                 <div className="links">
-                    <Link to="/">Where in the world?</Link>
-                    <button onClick={" "}>light mode</button>
+                    <Link className={`${theme}`} to="/">Where in the world?</Link>
+                    <FontAwesomeIcon icon={faMoon} className={`lightMode ${theme}`} onClick={toggleTheme}/>
                 </div>
             </nav>
     );
