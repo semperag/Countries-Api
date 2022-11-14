bf77f4cb251fc33b30258952129345d82e26e108
AdiSemper <semperag@dukes.jmu.edu>
2022-11-13 21:52:47 -0500

    loading screen

--- src/App.js
+++ src/App.js
@@ -9,36 +9,51 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import NotFound from './NotFound';
 import Country from './Country';
 
+
 function App() {
+
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  const handleLoading = () => {
+  setIsLoading(false);
+  }
+  
+  useEffect(()=>{
+  window.addEventListener("load",handleLoading);
+  return () => window.removeEventListener("load",handleLoading);
+  },[])
+  
   const [theme, setTheme] = useState('light');
   
   useEffect(() => {}, [theme]);
 
-  return (
+  return !isLoading ? (
     <div className={`body ${theme}`}>
-      <Router>
-        <Navbar theme={theme} setTheme={setTheme}/>
-          <div className="App">
-              <div className='content'>
-                <Switch>
-                  <Route exact path="/">
-                    <Home theme={theme}/>
-                  </Route>
-                  <Route path="/details/:id">
-                    <div className='single-country'>
-                      <Back theme={theme}/>
-                      <Country theme={theme}/>
-                    </div>
-                  </Route>
-                  <Route path="*">
-                    <NotFound />
-                  </Route>
-                </Switch>
-              </div>
-        </div>
-      </Router>
+    <Router>
+      <Navbar theme={theme} setTheme={setTheme}/>
+        <div className="App">
+            <div className='content'>
+              <Switch>
+                <Route exact path="/">
+                  <Home theme={theme}/>
+                </Route>
+                <Route path="/details/:id">
+                  <div className='single-country'>
+                    <Back theme={theme}/>
+                    <Country theme={theme}/>
+                  </div>
+                </Route>
+                <Route path="*">
+                  <NotFound />
+                </Route>
+              </Switch>
+            </div>
       </div>
-  );
+    </Router>
+    </div>
+  ):(<div className={`${theme}`}><div className='loader'></div></div>)
+    
+  
 }
 
 export default App;
