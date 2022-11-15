9f3adf595512ed7b537dd409ee1d8aa0a33b6ce4
AdiSemper <semperag@dukes.jmu.edu>
2022-11-14 23:21:18 -0500

    dark mode

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div className="not-found">
+        <div className="notFound">
             <h2>Sorry</h2>
             <p>That page cannot be found</p>
-            <Link to="/">Back to the homepage...</Link>
+            <p>404 Error</p>
         </div>
     )
 }
